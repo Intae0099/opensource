243d7acaddc57496da413df0838055255fd174d3
Intae0099 <81225723+Intae0099@users.noreply.github.com>
2022-04-06 13:22:06 +0900

    Update Mainactivity.java

--- 5주차/Project5_3/Mainactivity.java
+++ 5주차/Project5_3/Mainactivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("5-3 2018038033 김태인");
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT);
